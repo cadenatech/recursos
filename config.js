de12ebc0467c2a7f70e1973f25c178a5c796faba
cadena <darwinantonio11707@ie-cadenalasplayas.edu.co>
2026-02-20 17:55:55 -0500

    config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // Set this to your published Apps Script web app URL.
 // Example: https://script.google.com/macros/s/AKfycb.../exec
-var GAS_WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbxTxNMhU6DsxfnwbtqfLzafj9AvMDYMyDG0qd03vKW8M2grSpZtsjerwO5NtxeWCWbI/exec';
+var GAS_WEBAPP_URL = 'https://script.google.com/macros/s/AKfycbztNfvvBcZo1xyFY5t-ibP7yZZ7BMmTSVYPUy9vJq8Cwy_Use2YAGCmCX4fr3G9pXY/exec';
